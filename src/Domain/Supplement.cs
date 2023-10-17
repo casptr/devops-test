9f58ad40f68e98f1085fd50007c94568ce40470e
SandroSmet <sandro.smet@student.hogent.be>
2023-10-17 21:08:35 +0200

    Add: Supplements page layout + mock data
    Co-authored-by: Fredjeeeuuuhhh <frederic.detender@student.hogent.be>

--- src/Domain/Supplement.cs
+++ src/Domain/Supplement.cs
@@ -15,16 +15,26 @@ public class Supplement
         get => name;
         set => name = Guard.Against.NullOrWhiteSpace(value, nameof(Name));
     }
+
     private string description = default!;
     public string Description
     {
         get => description;
         set => description = Guard.Against.NullOrWhiteSpace(value, nameof(Description));
     }
+
+    private string category = default!;
+    public string Category
+    {
+        get => category;
+        set => category = Guard.Against.NullOrWhiteSpace(value,nameof(Category));
+    }
+
     private double price= default!;
     public double Price 
-    { get=>price; 
-      set=>price = Guard.Against.Negative(value, nameof(Price));
+    { 
+        get=>price; 
+        set=>price = Guard.Against.Negative(value, nameof(Price));
     }
 
     private string imageUrl = default!;
@@ -33,16 +43,18 @@ public class Supplement
         get => imageUrl;
         set => imageUrl = Guard.Against.NullOrWhiteSpace(value, nameof(ImageUrl));
     }
+
     private int amountAvailable=default!;
     public int AmountAvailable 
     { get=>amountAvailable; 
       set=>amountAvailable = Guard.Against.Negative(value,nameof(AmountAvailable)); 
     }
 
-    public Supplement(string name, string description, double price, string imageUrl, int amountAvailable) 
+    public Supplement(string name, string description, string category, double price, string imageUrl, int amountAvailable) 
     { 
         Name = name;
         Description = description;
+        Category = category;
         Price = price;
         ImageUrl = imageUrl;
         AmountAvailable = amountAvailable;
