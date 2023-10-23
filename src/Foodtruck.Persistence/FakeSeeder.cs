4883a74ce6978bb8dd5f806e6504c26cd48d5071
SandroSmet <sandro.smet@student.hogent.be>
2023-10-23 16:45:39 +0200

    Modify: layout formula responsivenes
    Co-authored-by: Fredjeeeuuuhhh <frederic.detender@student.hogent.be>

--- src/Foodtruck.Persistence/FakeSeeder.cs
+++ src/Foodtruck.Persistence/FakeSeeder.cs
@@ -26,19 +26,18 @@ public class FakeSeeder
     }
     private void SeedSupplements()
     {
-        var supplements = new SupplementFaker().AsTransient().UseSeed(101).Generate(14);
+        var supplements = new SupplementFaker().AsTransient().UseSeed(102).Generate(6);
         dbContext.Supplements.AddRange(supplements);
         dbContext.SaveChanges();
     }
 
     private void SeedFormulas()
     {
-        Random random = new Random();
-       
-        var formulas = new FormulaFaker().AsTransient().UseSeed(102).Generate(3);
+        int quantity = 0;
+        var formulas = new FormulaFaker().AsTransient().UseSeed(109).Generate(3);
         formulas.ForEach(f => {
            
-            var supplements = new SupplementFaker().AsTransient().UseSeed(101).Generate(14).Take(random.Next(1, 15));
+            var supplements = new SupplementFaker().AsTransient().UseSeed(102).Generate(6).Take(quantity+=2);
             foreach (var item in supplements)
             {
                 f.AddIncludedSupplement(item);
