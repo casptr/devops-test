4166fc02f8138a8b5e9cff42973a1c3d9079b74b
Tiê Proost <tie.proost@student.hogent.be>
2023-10-24 08:05:13 +0200

    refactor FormulasOverview placeholder logic

--- src/Client/Pages/FormulasOverview.razor.cs
+++ src/Client/Pages/FormulasOverview.razor.cs
@@ -16,10 +16,7 @@ public partial class FormulasOverview
     protected override async Task OnParametersSetAsync()
     {
         var response1 = await SupplementService.GetAllAsync();
-
         supplements = response1.Supplements;
-        names = supplements?.Select(s => s.Name).ToHashSet();
-
 
         var response = await FormulaService.GetAllAsync();
         formulas = response.Formulas;
