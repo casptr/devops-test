860b42ebb4841312ffd21158fbcd1f3e23310488
Pieter Vandewalle <67506292+PieterVandewalle@users.noreply.github.com>
2023-10-26 14:13:56 +0200

    Move Quotation process timeline to seperate file + add logic

---
+++ src/Client/QuotationProcess/QuotationProcessTimelineStep.cs
@@ -0,0 +1,22 @@
+﻿namespace Foodtruck.Client.QuotationProcess
+{
+    public class QuotationProcessTimelineStep
+    {
+        public QuotationProcessStep Step { get; set; }
+        public string StepTitle { get; set; }
+
+        public QuotationProcessTimelineStep(QuotationProcessStep step, string stepTitle)
+        {
+            Step = step;
+            StepTitle = stepTitle;
+        }
+    }
+
+    public enum QuotationProcessStep
+    {
+        CHOOSE_DATE,
+        SELECT_FORMULA,
+        ADD_SUPPLEMENTS,
+        ENTER_DETAILS,
+    }
+}
