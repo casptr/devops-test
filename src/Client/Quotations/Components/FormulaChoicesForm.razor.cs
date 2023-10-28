a4e075f5854998b543a9e0c8b6feceffc3fbab06
GiovanyDeMurel-HoGent <giovany.demurel@student.hogent.be>
2023-10-28 19:20:48 +0200

    Formule keuze formulier en Klantengegevens formulier

---
+++ src/Client/Quotations/Components/FormulaChoicesForm.razor.cs
@@ -0,0 +1,102 @@
+using Foodtruck.Shared.Formulas;
+using Foodtruck.Shared.Supplements;
+using Microsoft.AspNetCore.Components;
+
+
+namespace Foodtruck.Client.Quotations;
+
+public partial class FormulaChoicesForm
+{
+    [Inject] public IFormulaService FormulaService { get; set; } = default!;
+    [Inject] public ISupplementService SupplementService { get; set; } = default!;
+
+    private IEnumerable<FormulaDto.Detail>? formulas;
+    private List<string>? names=new();
+
+    private int numberOfGuests;
+
+    private IEnumerable<FormulaSupplementChoiceDto.Detail> selectedFormulaChoices = new List<FormulaSupplementChoiceDto.Detail>();
+    private IEnumerable<SupplementDto.Detail>? chosenSupplements = new List<SupplementDto.Detail>();
+
+    protected override async Task OnParametersSetAsync()
+    {
+        numberOfGuests = 1;
+        var response = await FormulaService.GetAllAsync();
+        formulas = response.Formulas;
+
+        var includedSupplementNames = formulas.SelectMany(formule => formule.IncludedSupplements.Select(supplementLine => supplementLine.Supplement.Name)).ToHashSet();
+        var choiceNames = formulas.SelectMany(formule => formule.Choices.Select(choice => choice.Name)).ToHashSet();
+        names.AddRange(choiceNames);
+        names.AddRange(includedSupplementNames);
+        // names.ForEach(n=>Console.WriteLine(n));
+
+        
+        if(formulas != null){
+            FormulaDto.Detail defaultFormula = formulas.First();
+            if(defaultFormula != null){
+            HandleSelectFormula(defaultFormula.Id);
+            }
+        }
+    }
+
+    private FormulaDto.Detail? selectedFormula;
+
+private void HandleSelectFormula(int formulaId)
+{
+    if (formulas != null)
+    {
+        FormulaDto.Detail? newSelectedFormula = formulas.FirstOrDefault(f => f.Id == formulaId);
+        if (newSelectedFormula != null)
+        {
+            if (selectedFormula == null || selectedFormula.Id != formulaId)
+            {
+                selectedFormula = newSelectedFormula;
+                selectedFormulaChoices = new List<FormulaSupplementChoiceDto.Detail>();
+                foreach(var supplementToChoose in selectedFormulaChoices){
+                    var appendedChosenSupplements = chosenSupplements?.Append(supplementToChoose.DefaultChoice);
+                }
+                
+                if (selectedFormula.Choices != null)
+                {
+                    foreach (var choice in selectedFormula.Choices)
+                    {
+                        var appendedChoices = selectedFormulaChoices.Append(choice);
+                    }
+                }
+            }
+        }
+    }
+}
+
+private Dictionary<int, string> choiceNames = new Dictionary<int, string>();
+private void HandleSelectSupplementChoice(int choicesId, int supplementChoiceId)
+{
+    if (selectedFormula != null && selectedFormula.Choices != null)
+    {
+        var selectedChoice = selectedFormula.Choices.First(c => c.Id == choicesId);
+
+        if (selectedChoice.SupplementsToChoose != null)
+        {
+            var selectedSupplement = selectedChoice.SupplementsToChoose.FirstOrDefault(s => s.Id == supplementChoiceId);
+
+            if (selectedSupplement != null)
+            {
+                choiceNames[choicesId] = selectedSupplement.Name;
+            }
+        }
+    }
+}
+
+
+
+
+    private void IncrementNumberOfGuests(){
+        numberOfGuests += 1;
+    }
+
+    private void DecrementNumberOfGuests(){
+        if(numberOfGuests > 1){
+            numberOfGuests -= 1;
+        }
+    }
+}
