77c6eace0ca548b3ba51b70d4ced171e1b63335f
Pieter Vandewalle <67506292+PieterVandewalle@users.noreply.github.com>
2023-10-31 19:09:56 +0100

    add admin login functionality

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Components.Web;
+﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Foodtruck.Client;
 using MudBlazor.Services;
@@ -18,16 +18,29 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddAuthorizationCore();
-builder.Services.AddSingleton<FakeAuthenticationProvider>();
-builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<FakeAuthenticationProvider>());
-builder.Services.AddTransient<FakeAuthorizationMessageHandler>();
+
+// Auth0
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+});
+// Old auth
+
+// builder.Services.AddSingleton<FakeAuthenticationProvider>();
+// builder.Services.AddAuthorizationCore();
+// builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<FakeAuthenticationProvider>());
+// builder.Services.AddTransient<FakeAuthorizationMessageHandler>();
+
 builder.Services.AddTransient<CleanErrorHandler>();
 
 builder.Services.AddHttpClient("Project.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<CleanErrorHandler>();
 
+
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Project.ServerAPI"));
+
+
 
 //builder.Services.AddScoped<IFormulaService, FormulaService>();
 builder.Services.AddScoped<IFormulaService, FakeFormulaService>();
