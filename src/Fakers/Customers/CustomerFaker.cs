2fb33bab82a8131e082450f9cf61a81cffa9e80e
Pieter Vandewalle <67506292+PieterVandewalle@users.noreply.github.com>
2023-11-01 13:43:33 +0100

    fix broken build

--- src/Fakers/Customers/CustomerFaker.cs
+++ src/Fakers/Customers/CustomerFaker.cs
@@ -8,6 +8,6 @@ public class CustomerFaker : EntityFaker<Customer>
 	private static int id = 100;
 	public CustomerFaker(string locale = "nl") : base(locale)
 	{
-		CustomInstantiator(f => new Customer(f.Person.FirstName, f.Person.LastName, new EmailAddress(f.Person.Email), f.Person.Phone, f.Company.CompanyName(), f.Company.Random.String(10), f.Random.Bool())).RuleFor(f => f.Id, s => id++);
+		CustomInstantiator(f => new Customer(f.Person.FirstName, f.Person.LastName, new EmailAddress(f.Person.Email), f.Person.Phone, f.Company.CompanyName(), f.Company.Random.String(10))).RuleFor(f => f.Id, s => id++);
 	}
 }
