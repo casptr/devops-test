2427ba0c6ffc62971f316de87a1b9292d55ff2b7
Jeff Wellekens <jeff.wellekens@student.hogent.be>
2023-11-03 16:57:06 +0100

    layout overzicht supplementen toevoegen

--- src/Client/QuotationProcess/AddSupplements.razor.cs
+++ src/Client/QuotationProcess/AddSupplements.razor.cs
@@ -5,38 +5,45 @@ using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Linq;
 
-namespace Foodtruck.Client.QuotationProcess
+namespace Foodtruck.Client.QuotationProcess;
+
+public partial class AddSupplements
 {
-    public partial class AddSupplements
+    [Inject] public QuotationProcessState QuotationProcessState { get; set; } = default!;
+    [Inject] public ISupplementService SupplementService { get; set; } = default!;
+
+    private IEnumerable<ExtraSupplement>? supplements;
+
+	bool open = true;
+
+	void ToggleStartDrawer()
+	{
+		open = !open;
+	}
+
+	protected override async Task OnParametersSetAsync()
     {
-        [Inject] public QuotationProcessState QuotationProcessState { get; set; } = default!;
-        [Inject] public ISupplementService SupplementService { get; set; } = default!;
-
-        private IEnumerable<ExtraSupplement>? supplements;
-        protected override async Task OnParametersSetAsync()
+        var response = await SupplementService.GetAllAsync();
+        Console.WriteLine(response.Supplements);
+        supplements = response.Supplements?.Select(supplement =>
         {
-            var response = await SupplementService.GetAllAsync();
-            Console.WriteLine(response.Supplements);
-            supplements = response.Supplements?.Select(supplement =>
-            {
-                ExtraSupplement? extraSupplement = QuotationProcessState.SupplementChoices.Where(supplementInState => supplementInState.Equals(supplement)).FirstOrDefault();
-                return new ExtraSupplement()
-                {
-                    Supplement = supplement,
-                    Quantity = extraSupplement is null ? 0 : extraSupplement.Quantity,
-                };
-            }).ToList();
-        }
-
-        private void AddSupplement(SupplementDto.Detail supplement, int quantity)
-        {
-            var supplementChoice = new ExtraSupplement()
+            ExtraSupplement? extraSupplement = QuotationProcessState.SupplementChoices.Where(supplementInState => supplementInState.Equals(supplement)).FirstOrDefault();
+            return new ExtraSupplement()
             {
                 Supplement = supplement,
-                Quantity = quantity
+                Quantity = extraSupplement is null ? 0 : extraSupplement.Quantity,
             };
+        }).ToList();
+    }
 
-            QuotationProcessState.AddSupplement(supplementChoice);
-        }
+    private void AddSupplement(SupplementDto.Detail supplement, int quantity)
+    {
+        var supplementChoice = new ExtraSupplement()
+        {
+            Supplement = supplement,
+            Quantity = quantity
+        };
+
+        QuotationProcessState.AddSupplement(supplementChoice);
     }
 }
