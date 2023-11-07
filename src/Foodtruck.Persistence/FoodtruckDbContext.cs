d44814820ca9e688c2bc982823b14b4f63b28dcd
Tiê Proost <tie.proost@student.hogent.be>
2023-11-07 19:33:25 +0100

    add FoodtruckDbContext

---
+++ src/Foodtruck.Persistence/FoodtruckDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Customers;
+using Domain.Formulas;
+using Domain.Quotations;
+using Domain.Supplements;
+using Microsoft.EntityFrameworkCore;
+
+namespace Foodtruck.Persistence
+{
+    public class FoodtruckDbContext : DbContext
+    {
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<MarketingSubscriber> MarketingSubscriber { get; set; }
+
+        public DbSet<Supplement> Supplement { get; set; }
+        public DbSet<Formula> Formula { get; set; }
+        public DbSet<Quotation> Quotation { get; set; }
+
+        public FoodtruckDbContext(DbContextOptions<FoodtruckDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(FoodtruckDbContext).Assembly);
+        }
+    }
+}
